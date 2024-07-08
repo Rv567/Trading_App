2b36c04ff6e403e2811b80ec57245ec627acf860
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-08 17:19:54 +0100

    indi

--- Functions/indicators.py
+++ Functions/indicators.py
@@ -1,17 +1,17 @@
 import pandas_ta as ta
 
 
-#Momentum Indicators (7)
+"""#Momentum Indicators (7)
 
 def ADX(dataFrame): #Average Directional Movement Index
     dataFrame['ADX'] = ta.ADX(dataFrame.High, dataFrame.Low, dataFrame.Close, timeperiod=14)
 
 def MACD(dataFrame):#Moving Average Convergence Divergence
-    """
+    
     MACD = fast_MA(12) - slow_MA(26)
     Signal = EMA9(MACD)
     Hist = MACD - Signal
-    """
+    
     dataFrame['MACD'],dataFrame['MACD_Signal'],dataFrame['MACD_Hist'] = ta.MACD(dataFrame.Close, fastperiod=12, slowperiod=26, signalperiod=9)
 
 def RSI(dataFrame):#RSI(14)
@@ -68,4 +68,75 @@ def ADOSC (dataFrame):#Chaikin A/D Oscillator
     dataFrame["ADOSC "]= ta.ADOSC (dataFrame.High, dataFrame.Low, dataFrame.Close, dataFrame.Volume,fastperiod=3, slowperiod=10)
 
 def OBV(dataFrame):#On Balance Volume
-    dataFrame["OBV"]= ta.OBV(dataFrame.Close, dataFrame.Volume)
+    dataFrame["OBV"]= ta.OBV(dataFrame.Close, dataFrame.Volume)"""
+
+
+# Momentum Indicators (7)
+
+def ADX(dataFrame): # Average Directional Movement Index
+    dataFrame['ADX'] = ta.adx(dataFrame.High, dataFrame.Low, dataFrame.Close)['ADX_14']
+
+def MACD(dataFrame): # Moving Average Convergence Divergence
+    macd = ta.macd(dataFrame.Close, fast=12, slow=26, signal=9)
+    dataFrame['MACD'] = macd['MACD_12_26_9']
+    dataFrame['MACD_Signal'] = macd['MACDs_12_26_9']
+    dataFrame['MACD_Hist'] = macd['MACDh_12_26_9']
+
+def RSI(dataFrame): # RSI(14)
+    dataFrame['RSI'] = ta.rsi(dataFrame.Close, length=14)
+
+def CCI(dataFrame): # Commodity Channel Index
+    dataFrame['CCI'] = ta.cci(dataFrame.High, dataFrame.Low, dataFrame.Close, length=14)
+
+def STOCH(dataFrame): # Stochastic
+    stoch = ta.stoch(dataFrame.High, dataFrame.Low, dataFrame.Close, k=5, d=3)
+    dataFrame["slowk"] = stoch['STOCHk_5_3_3']
+    dataFrame["slowd"] = stoch['STOCHd_3_3']
+
+def ROC(dataFrame): # Rate of Change
+    dataFrame["ROC"] = ta.roc(dataFrame.Close, length=10)
+
+def WR(dataFrame): # Williams' %R
+    dataFrame['WilliamsR'] = ta.willr(dataFrame.High, dataFrame.Low, dataFrame.Close, length=14)
+
+# Overlap Indicators (6)
+
+def BBANDS(dataFrame): # Bollinger Bands
+    bbands = ta.bbands(dataFrame.Close, length=5, std=2)
+    dataFrame["upperband"] = bbands['BBU_5_2.0']
+    dataFrame["middleband"] = bbands['BBM_5_2.0']
+    dataFrame["lowerband"] = bbands['BBL_5_2.0']
+
+def DEMA(dataFrame, period): # Double Exponential Moving Average 
+    dataFrame["DEMA"] = ta.dema(dataFrame.Close, length=period)
+
+def EMA(dataFrame, period): # Exponential Moving Average
+    dataFrame[f"EMA{period}"] = ta.ema(dataFrame.Close, length=period)
+
+def MA(dataFrame, period): # Moving Average
+    dataFrame[f"MA{period}"] = ta.sma(dataFrame.Close, length=period)
+
+def SAR(dataFrame): # Parabolic SAR
+    dataFrame["SAR"] = ta.psar(dataFrame.High, dataFrame.Low, dataFrame.Close)['PSARl_0.02_0.2']
+
+def SMA(dataFrame, period): # Simple Moving Average
+    dataFrame[f"SMA{period}"] = ta.sma(dataFrame.Close, length=period)
+
+# Volatility Indicators (2)
+
+def ATR(dataFrame): # Average True Range
+    dataFrame["ATR"] = ta.atr(dataFrame.High, dataFrame.Low, dataFrame.Close, length=14)
+
+def NATR(dataFrame): # Normalized Average True Range
+    dataFrame["NATR"] = ta.natr(dataFrame.High, dataFrame.Low, dataFrame.Close, length=14)
+
+# Volume Indicators (3)
+
+def AD(dataFrame): # Chaikin A/D Line
+    dataFrame["AD"] = ta.ad(dataFrame.High, dataFrame.Low, dataFrame.Close, dataFrame.Volume)
+
+def ADOSC(dataFrame): # Chaikin A/D Oscillator
+    dataFrame["ADOSC"] = ta.adosc(dataFrame.High, dataFrame.Low, dataFrame.Close, dataFrame.Volume, fast=3, slow=10)
+
+def OBV(dataFrame): # On Balance Volume
+    dataFrame["OBV"] = ta.obv(dataFrame.Close, dataFrame.Volume)
