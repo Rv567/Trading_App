29b8070b83d87ec7d0799b4eaf3c17b3dee94403
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-11 02:43:43 +0100

    ff

--- page2.py
+++ page2.py
@@ -23,10 +23,10 @@ def app():
     #st.write(dataframes[stock_symbol].tail(10))
 
     st.subheader(f'Historical Closing Prices for {stock_symbol}')
-    st.write(dataframes[stock_symbol].index)
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(
-        x=dataframes[stock_symbol]["Date"]),
+        x=dataframes[stock_symbol].index),
         y=dataframes[stock_symbol]["Close"],
         mode='lines',
         line=dict(color=stock_color[stock_symbol])
