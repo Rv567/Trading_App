17aec74994eb6613a444ba849d32a1f2b10f02f1
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-11 02:43:11 +0100

    ff

--- page2.py
+++ page2.py
@@ -23,7 +23,7 @@ def app():
     #st.write(dataframes[stock_symbol].tail(10))
 
     st.subheader(f'Historical Closing Prices for {stock_symbol}')
-    st.write(dataframes[stock_symbol].columns)
+    st.write(dataframes[stock_symbol].index)
     fig = go.Figure()
     fig.add_trace(go.Scatter(
         x=dataframes[stock_symbol]["Date"]),
