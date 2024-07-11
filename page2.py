5e163b16de0e3b7e50d588d4d187111e16db706c
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-11 02:37:56 +0100

    tt

--- page2.py
+++ page2.py
@@ -23,6 +23,7 @@ def app():
     #st.write(dataframes[stock_symbol].tail(10))
 
     st.subheader(f'Historical Closing Prices for {stock_symbol}')
+    st.write(dataframes[stock_symbol])
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
