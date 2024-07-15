f1ff8a8eb8303abebe72694faddb3d0eebe6f505
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-15 18:34:21 +0100

    ff

--- page3.py
+++ page3.py
@@ -158,9 +158,10 @@ def app():
 
     # Rename the columns to the first row and then drop the first row
     selected_columns = ['EntryTime', 'EntryPrice', 'ReturnPct']
+    df_transposed = df_transposed[selected_columns]
     df_transposed=df_transposed.rename(columns={"EntryTime":"Entry Time","EntryPrice":"Entry Price","ReturnPct":"Perf %"})
     st.write(df_transposed.columns)
-    st.write(df_transposed[selected_columns])
+    st.write(df_transposed)
 
 
     # Select only the columns 'EntryTime', 'EntryPrice', 'ReturnPct'
