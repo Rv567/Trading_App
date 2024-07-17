2eb56fcdb41eb802b1034144993632caa58abfc7
Rv567 <chentouf-fcb@hotmail.fr>
2024-07-17 18:21:07 +0100

    test

--- page3.py
+++ page3.py
@@ -61,6 +61,7 @@ def app():
     low_volatility_df_stocks = low_volatility_df["Key"].tolist()
     dff = st.session_state['Newdict_df']
     st.write(dff.keys())
+    st.write(low_volatility_df_stocks)
 
 
     
