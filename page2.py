0a486e63b775df770b26e02a3037b7b7506230fd
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-01 12:42:32 +0100

    volume

--- page2.py
+++ page2.py
@@ -85,7 +85,7 @@ def app():
     st.session_state['Newdict_df'] = Newdict_df # store it for page2
     st.markdown("""
     **Volume Calculation:**
-    - The average volume of a stock is calculated using a 30-day rolling mean (dynamic average of previous 30 days).
+    - The average volume of a stock is calculated using a 252-day rolling mean (dynamic average of previous 252 days).
     """)
 
     st.subheader("Stocks meeting the liquidity criteria")
