eb586cdae97db0c3da4bcea7114d7c7ecc477cb2
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-01 15:02:15 +0100

    new page

--- page6.py
+++ page6.py
@@ -19,7 +19,7 @@ def app():
     
     #We load our entire data from pickle files
     dataframes = load_data()
-    ticker = ["MASI","ATW","IAM","BCP","LHM","BOA","TQM",'MNG',"CMA",'MSA','CSR','WAA','GAZ','LBV',"TMA",'CIH',"ADH","AKT","TGC","CDM","BCI","SAH","ATL",'LES',"ARD","ADI","DHO",'HPS','RIS',"ATH","SID","RDS","JET","SNA"]
+    ticker = ["MASI","ATW","IAM","BCP","LHM","BOA","TQM",'MNG',"CMA",'MSA','CSR','WAA','GAZ','LBV',"TMA",'CIH',"ADH","AKT","TGC","CDM","BCI","SAH","ATL",'LES',"ARD","CFG","ADI","DHO",'HPS','RIS',"ATH","SID","RDS","JET","SNA"]
     dataframes = {key: reorganize2(df, ticker[idx]) for idx, (key, df) in enumerate(dataframes.items())}
 
     df = pd.concat([dataframes[elem][elem] for elem in ticker],join="outer",axis=1,sort=True)
