7dbd4ddf4b333435ab28ad81aaff80fafc1fe59e
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-01 15:06:59 +0100

    opt

--- page6.py
+++ page6.py
@@ -32,9 +32,19 @@ def app():
         df[elem] = df[elem].pct_change()
 
     df.dropna(inplace=True)
+
     st.write(df)
 
     if st.button("Apply Metric"):
         if metric_choice == "Cumulative Return%":
             st.write(f"Results for {metric_choice}:")
+            
+            cum_returns = {}
+            for elem in df.columns:
+                individual_cumsum = ((1+df[elem]).cumprod()-1)*100
+                cum_returns[elem] = individual_cumsum[-1]
+
+            df_cum_returns = pd.DataFrame(list(cum_returns.items()), columns=['Stock', 'Cumulative Return%']).set_index('Stock')
+            df_cum_returns.sort_values(by="Cumulative Return%", ascending=False)
+            st.write(df_cum_returns)
     
