59d662642c6d9733024e6f90e7f2c700a6a99af6
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-02 16:00:43 +0100

    trace

--- page6.py
+++ page6.py
@@ -155,7 +155,7 @@ def app():
                     poids = df_poids_opt.loc[elem].values
                     optimized_portfolio += poids/100 * df[elem]
 
-            qs.stats.sharpe(df["MASI"], periods=252)
+            st.write(qs.stats.sharpe(df["MASI"], periods=252))
             #Plot
             trace_perf(optimized_portfolio,df["MASI"])
 
