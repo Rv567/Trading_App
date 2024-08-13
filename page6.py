0f9c5862efe7440c6241d0eb013370fbd28e4fd8
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-13 15:51:34 +0100

    portfolio

--- page6.py
+++ page6.py
@@ -342,3 +342,25 @@ def app():
             #Addictional metrics
             st.write("Additional Metrics :")
             metrics(optimized_portfolio,df["MASI"])
+
+    st.header("Own Portfolio Optimization") 
+
+    data_df = pd.DataFrame(
+        {
+            "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+            "favorite": [True, False, False, True],
+        }
+    )
+
+    st.data_editor(
+        data_df,
+        column_config={
+            "favorite": st.column_config.CheckboxColumn(
+                "Your favorite?",
+                help="Select your **favorite** widgets",
+                default=False,
+            )
+        },
+        disabled=["widgets"],
+        hide_index=True,
+    )
