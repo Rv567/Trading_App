c8847fd3b1ec8078f0fd47e8aed076b9bac7c234
Rv567 <chentouf-fcb@hotmail.fr>
2024-08-13 12:51:45 +0100

    solvency

--- page2.py
+++ page2.py
@@ -220,14 +220,14 @@ def app():
 
     # Create a query for the Moroccan stock exchange
     query = (Query()
-            .select('name', 'close', "Change %","Net Income (Annual YoY Growth)", "Return on Equity (TTM)","current_ratio", "Price to Earnings Ratio (TTM)", 'Price to Book (FY)','dividends_yield',
-                "Perf.YTD")
+            .select('name', 'close', "Change %","Net Income (Annual YoY Growth)", "Return on Equity (TTM)","current_ratio", "debt_to_equity_ratio","Price to Earnings Ratio (TTM)",'dividends_yield',
+                "Perf.YTD",)
             .order_by('market_cap_basic', ascending=False))  # Sort by market cap in descending order
     query.url = morocco_url
     count, df = query.get_scanner_data()
 
     df.drop(columns=["ticker"],inplace=True)
-    df = df.rename(columns={"name":"Name","close":"Close","change":"Change %","price_earnings_ttm":"P/E","price_book_ratio":"P/B","dividends_yield":"Div Yield %","net_income_yoy_growth_fy":"Net Income Growth %","Perf.YTD":"Perf %","return_on_equity":"ROE %","current_ratio":"Current Ratio"})
+    df = df.rename(columns={"name":"Name","close":"Close","change":"Change %","price_earnings_ttm":"P/E","dividends_yield":"Div Yield %","net_income_yoy_growth_fy":"Net Income Growth %","Perf.YTD":"Perf %","return_on_equity":"ROE %","current_ratio":"Current Ratio","debt_to_equity_ratio":"Debt/equity"})
     df = df.applymap(lambda x: round(x, 2) if isinstance(x, (int, float)) else x)
     df["Sector"] = ["Bank","Telecom","Bank","Materials","Bank","Materials","Utilities","Transportation","Materials","Food,Beverage","Insurance","Energy","Consumer Retailing","Healthcare","Energy","Bank","Real Estate","Capital Goods","Bank","Bank aside","Insurance","Insurance","Food,Beverage","Food,Beverage","Pharmaceuticals","Bank aside","Real Estate","Real Estate","Capital Goods","Diversified Financials","Materials","Consumer Services","Retail","Materials","Food,Beverage","Materials","Food,Beverage","Real Estate","Retail","Diversified Financials","Capital Goods","Diversified Financials","Real Estate","Tech","Insurance","Insurance","Materials","Tech","Food,Beverage","Pharmaceuticals"]
     st.write(df)
