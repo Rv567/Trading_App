88c65507ba51f58c063e753e4090bf9ead9ff002
Rv567 <chentouf-fcb@hotmail.fr>
2025-02-11 22:12:36 +0100

    lib

--- Functions/mylibraries.py
+++ Functions/mylibraries.py
@@ -50,6 +50,7 @@ from IPython.display import display
 import warnings
 warnings.filterwarnings("ignore")
 
+from multiprocessing import Pool, cpu_count
 from tradingview_screener import Query, Column
 import yaml
 from yaml.loader import SafeLoader
