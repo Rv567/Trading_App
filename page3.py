7e74f6412a0e6c0787a3c478407e0d9b9920e6a1
Rv567 <chentouf-fcb@hotmail.fr>
2025-02-11 09:34:13 +0100

    print

--- page3.py
+++ page3.py
@@ -150,7 +150,7 @@ def app():
     # Beta>1
     st.subheader("Corresponding Stocks performance for an **:green[uptrend market]**")
     df_high = pd.read_pickle('performance_high.pkl')
-    st.write(df_high)
+    #st.write(df_high)
     df_high_mod,date = modify_big(df_high)
     st.write(f"The backtest begins from from the start date of each stock to {date}.")
     st.write(df_high_mod)
