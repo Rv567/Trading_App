d598b82b820a375919987d85bb3235c686928a09
Rv567 <chentouf-fcb@hotmail.fr>
2025-02-26 13:07:23 +0100

    comm

--- page5.py
+++ page5.py
@@ -6,6 +6,7 @@ def app():
     st.header("Introduction to the statistical analysis and its importance")
     st.write("Welcome to the Statistical Data Analysis page. Here, we analyze correlations between different stocks to identify pairs with strong relationships.")
 
+    # Load stock data from session state
     df = st.session_state['Newdict_df']
     Newdict_df_close = {key:value["Close"] for key,value in df.items()}
     #Newdict_df_return = {key:value["Close"].pct_change() for key,value in df.items()}
@@ -14,7 +15,7 @@ def app():
     df = pd.DataFrame(Newdict_df_close)
     df.index = pd.to_datetime(df.index)
     correlation_matrix = df.corr()
-
+    # Correlation Heatmap
     mask = np.zeros_like(correlation_matrix, dtype=bool)
     mask[np.triu_indices_from(mask)] = True
     st.header("Correlation Calculation")    
@@ -29,7 +30,7 @@ def app():
     - **Negative Correlation**: When the prices of two stocks move in opposite directions.
     - **No Correlation**: When there is no discernible pattern in the price movements of two stocks.
     """)
-    
+
     st.header("Couples with Strong Correlation")
     st.write("Select the correlation threshold.")
     threshold = st.slider('Threshold :', min_value=80, max_value=100, value=90, step=1)
@@ -42,15 +43,15 @@ def app():
             if correlation_matrix.iloc[i, j] > threshold:
                 high_corr_pairs.append((correlation_matrix.columns[i], correlation_matrix.columns[j], correlation_matrix.iloc[i, j]))
 
-    # Print the pairs
     for pair in high_corr_pairs:
         st.write(f"Pair: {pair[0]} and {pair[1]} with correlation {pair[2]}")
 
+    # Time-Series Analysis for Selected Correlated Pairs
     st.header("Time-Series Analysis")
     st.write("Select the two pairs from the high correaltion pairs list.")
     pairs = st.selectbox('Select pairs', high_corr_pairs)
-    #plot_scatter_pairs(Newdict_df_close, pairs[0],pairs[1])
 
+    # Historical stock prices for the selected pair
     st.write("We plot the historical prices of selected stocks.")
 
     fig = go.Figure()
@@ -79,10 +80,13 @@ def app():
     )
     
     st.plotly_chart(fig,use_container_width=True)
-    ##################################################
+
+    ################################################## Pairs Trading Analysis
     st.header("Pairs Trading")
     st.write("Pairs trading involves identifying two stocks with a strong historical correlation and exploiting the temporary divergences in their price relationship.")
     st.write("We start with calculating the spread (price difference the two stocks), then we normalize it to obtain the z-score.")
+    
+    # Z-score Calculation
     st.subheader("Z-score Calculating")
     df = pd.merge(Newdict_df_close[pairs[0]],Newdict_df_close[pairs[1]],left_index=True, right_index=True, suffixes=(f'_{pairs[0]}', f'_{pairs[1]}'))
     
@@ -92,10 +96,10 @@ def app():
 
     df['hedge_ratio'] = model.params[1]
 
-    # Calculate the spread
+    # Spread
     df['spread'] = df[f"Close_{pairs[0]}"] - df['hedge_ratio'] * df[f"Close_{pairs[1]}"]
 
-    # Calculate z-score
+    # Z-score
     df['z_score'] = (df['spread'] - df['spread'].mean()) / df['spread'].std()
     
     st.write(df['z_score'].tail(20))
